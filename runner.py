5a2d5255cde3fa8752ea53389c5b23c9c84ec01e
av <bobopirate1010@gmail.com>
2017-12-12 21:06:20 -0800

    Add code to detect reflective tape as well as form a bounding rectangle. Not release-ready yet.

--- runner.py
+++ runner.py
@@ -1,30 +1,44 @@
-
-
-
-
 import numpy as np
 import cv2
 from greenCross import GripGreenCross
+from reflectiveTape import ReflectivePipeline
+
+#camera settings
+#-1 finds the first camera, so ensure only 1 camera is in
 cap = cv2.VideoCapture(1)
 cap.set(cv2.cv.CV_CAP_PROP_FPS, 60)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640);
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,480);
-green = GripGreenCross()
+
+#pipeline processor object
+og = ReflectivePipeline()
+
 while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    green.process(frame)
-    mask = green.mask_output
+    # Pass in frame from camera (cap)
+    _, frame = cap.read()
+    og.process(frame)
     
-    contour = green.filter_contours_output
-
-    # Our operations on the frame come here
-
-
-    # Display the resulting frame
+    #set mask and countour frames
+    mask = og.mask_output
+    contour = og.filter_contours_output
+    if len(contour) > 0:
+		cnt = contour[0]
+		#rectangle approximation code (opencv 3.0+ code)
+		#rect = cv2.minAreaRect(cnt)
+		#box = cv2.boxPoints(rect)
+		#box = np.int0(box)
+		#cv2.drawContours(frame, [box], 0, (0,0,255), 2)
+    
+    
+    
+	#show countour on mask
     cv2.drawContours(mask,	 contour,-1,(0,0,255),3) 
+    
+	#show frames mask and default frame
     cv2.imshow('mask', mask)
     cv2.imshow('frame',frame)
+    
+    #check if q is hit to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 

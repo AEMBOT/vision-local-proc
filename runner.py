a708cfcd2feb5a5dd371e0c190399f158283d5b7
Alex <bobopirate1010@gmail.com>
2018-02-11 22:09:45 -0800

    Add code to detect power cube for vision 2018 just in case.

--- runner.py
+++ runner.py
@@ -2,46 +2,51 @@ import numpy as np
 import cv2
 from greenCross import GripGreenCross
 from reflectiveTape import ReflectivePipeline
+from powerCube import GripPipeline
 
-#camera settings
-#-1 finds the first camera, so ensure only 1 camera is in
+# camera settings
+# -1 finds the first camera, so ensure only 1 camera is in
 cap = cv2.VideoCapture(1)
-cap.set(cv2.cv.CV_CAP_PROP_FPS, 60)
-cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640);
-cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,480);
+cap.set(cv2.CAP_PROP_FPS, 60)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640);
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480);
 
-#pipeline processor object
-og = ReflectivePipeline()
+# pipeline processor object
+pipeline = GripPipeline()
 
-while(True):
+
+while (True):
     # Pass in frame from camera (cap)
-    _, frame = cap.read()
-    og.process(frame)
-    
-    #set mask and countour frames
-    mask = og.mask_output
-    contour = og.filter_contours_output
+   # ret, frame = cap.read()
+    ret = cv2.imread('powercube.png', 1)
+
+    print('f')
+    pipeline.process(ret)
+
+    # set mask and countour frames
+    mask = pipeline.mask_output
+    contour = pipeline.filter_contours_output
     if len(contour) > 0:
-		cnt = contour[0]
-		#rectangle approximation code (opencv 3.0+ code)
-		#rect = cv2.minAreaRect(cnt)
-		#box = cv2.boxPoints(rect)
-		#box = np.int0(box)
-		#cv2.drawContours(frame, [box], 0, (0,0,255), 2)
-    
-    
-    
-	#show countour on mask
-    cv2.drawContours(mask,	 contour,-1,(0,0,255),3) 
-    
-	#show frames mask and default frame
+        cnt = contour[0]
+    # rectangle approximation code (opencv 3.0+ code)
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(ret, [box], 0, (0,0,255), 2)
+
+    # show countour on mask
+    cv2.drawContours(mask, contour, -1, (0, 0, 255), 3)
+
+    # show frames mask and default frame
     cv2.imshow('mask', mask)
-    cv2.imshow('frame',frame)
-    
-    #check if q is hit to quit
+    cv2.imshow('frame', ret)
+    cv2.imshow('frame', ret)
+
+
+    # check if q is hit to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+    print("t")
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
